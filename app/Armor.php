cd9c3850cfe0aaf8f76dcd2bcd67a4f7924b1fe6
Jared David <skynet@jareds-mbp.attlocal.net>
2016-08-06 13:12:39 -0400

    Fixing the ability to create

--- app/Armor.php
+++ app/Armor.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Armor extends Model
 {
-    //protected $fillable = ['item_template_id','character_id','uses'];
+    protected $fillable = [
+    	'armor_template_id','character_id','defense',
+		'fireres','waterres','earthres',
+		'lightningres','windres', 'created_at'
+	];
+	public $timestamps = false;
+	
     
     public function character()
     {
