9fe7b18472ca6cc7fd28bf3227f684cf82a2f42a
Jared David <skynet@Jareds-MacBook-Pro.local>
2016-08-27 13:56:09 -0400

    Appearance changes

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,200italic,300italic" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="http://d3dhju7igb20wy.cloudfront.net/assets/0-4-0/all-the-things.css" />
+    
     <style>
         .chat-app {
             margin: 50px;
