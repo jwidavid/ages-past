d3ad954603ebb41c8c2962289c047c72e0c74de5
Jared David <skynet@Jareds-MacBook-Pro.local>
2016-08-27 02:03:42 -0400

    Chat attempt 2

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,72 +1,107 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="language" content="en"/>
-
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/chat/main.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/chat/bootstrap.min.css') }}" />
-    <script type="text/javascript" src="{{ URL::asset('js/chat/jquery.min.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('js/chat/bootstrap.min.js') }}"></script>
-    <title>demo chat</title>
-    <script>
-        $(function(){
-            function wsStart() {
-                ws = new WebSocket("ws://127.0.0.1:8001/");
-                ws.onopen = function() {
-                    $("#chat").append("<p>system: connection is open. Please enter a name that will be displayed. The name can be used English letters and numbers. The name must not exceed 10 characters.</p>");
-                }
-                ws.onclose = function() {
-                    $("#chat").append("<p>system: the connection is closed, I try to reconnect</p>");
-                    $("#login p").remove();
-                    $('#login span').text('0');
-                    setTimeout(wsStart, 1000);
-                }
-                ws.onmessage = function(evt) {
-                    var pack = JSON.parse(evt.data);
-                    if (pack.cmd == 'message') {
-                        $("#chat").append("<p>"+pack.data+"</p>");
-                        $('#chat').scrollTop($('#chat')[0].scrollHeight);
-                    } else if (pack.cmd == 'login') {
-                        $("#login").append("<p id='l_" + pack.data + "'>" + pack.data + "</p>");
-                        $('#login span').text($('#login p').size());
-                        $('#login').scrollTop($('#login')[0].scrollHeight);
-                    } else if (pack.cmd == 'logout') {
-                        $("#l_" + pack.data).remove();
-                        $('#login span').text($('#login p').size());
-                        $('#login').scrollTop($('#login')[0].scrollHeight);
-                    } else if (pack.cmd == 'logins') {
-                        pack.data.forEach(function(login) {
-                            $("#login").append("<p id='l_" + login + "'>" + login + "</p>");
-                        })
-                        $('#login span').text($('#login p').size());
-                        $('#login').scrollTop($('#login')[0].scrollHeight);
-                    }
-                }
-            }
-            wsStart();
-            $('#chat').height($(window).height() - 80);
-            $('#login').height($(window).height() - 80);
-            $('#input').focus();
-        });
-    </script>
+<meta charset='UTF-8' />
+<style type="text/css">
+<!--
+.chat_wrapper {
+	width: 500px;
+	margin-right: auto;
+	margin-left: auto;
+	background: #CCCCCC;
+	border: 1px solid #999999;
+	padding: 10px;
+	font: 12px 'lucida grande',tahoma,verdana,arial,sans-serif;
+}
+.chat_wrapper .message_box {
+	background: #FFFFFF;
+	height: 150px;
+	overflow: auto;
+	padding: 10px;
+	border: 1px solid #999999;
+}
+.chat_wrapper .panel input{
+	padding: 2px 2px 2px 5px;
+}
+.system_msg{color: #BDBDBD;font-style: italic;}
+.user_name{font-weight:bold;}
+.user_message{color: #88B6E0;}
+-->
+</style>
 </head>
+<body>	
+<?php 
+$colours = array('007AFF','FF7000','FF7000','15E25F','CFC700','CFC700','CF1100','CF00BE','F00');
+$user_colour = array_rand($colours);
+?>
 
-<body>
-<div class="container-fluid">
-    <div class="row">
-        <div style="float: left;width: 85%;padding: 10px;">
-            <div id="chat" style="overflow: auto;" class="well"><p>system: please wait, I try to connect to the server.</p></div>
-        </div>
-        <div style="width: 15%;margin-left: 85%; padding: 10px;">
-            <div id="login" style="overflow: auto;" class="well"><b>Users (<span>0</span>):</b></div>
-        </div>
-    </div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+<script language="javascript" type="text/javascript">  
+$(document).ready(function(){
+	//create a new WebSocket object.
+	var wsUri = "ws://localhost:9000/serve.php"; 	
+	websocket = new WebSocket(wsUri); 
+	
+	websocket.onopen = function(ev) { // connection is open 
+		$('#message_box').append("<div class=\"system_msg\">Connected!</div>"); //notify user
+	}
+
+	$('#send-btn').click(function(){ //use clicks message send button	
+		var mymessage = $('#message').val(); //get message text
+		var myname = $('#name').val(); //get user name
+		
+		if(myname == ""){ //empty name?
+			alert("Enter your Name please!");
+			return;
+		}
+		if(mymessage == ""){ //emtpy message?
+			alert("Enter Some message Please!");
+			return;
+		}
+		
+		//prepare json data
+		var msg = {
+		message: mymessage,
+		name: myname,
+		color : '<?php echo $colours[$user_colour]; ?>'
+		};
+		//convert and send data to server
+		websocket.send(JSON.stringify(msg));
+	});
+	
+	//#### Message received from server?
+	websocket.onmessage = function(ev) {
+		var msg = JSON.parse(ev.data); //PHP sends Json data
+		var type = msg.type; //message type
+		var umsg = msg.message; //message text
+		var uname = msg.name; //user name
+		var ucolor = msg.color; //color
+
+		if(type == 'usermsg') 
+		{
+			$('#message_box').append("<div><span class=\"user_name\" style=\"color:#"+ucolor+"\">"+uname+"</span> : <span class=\"user_message\">"+umsg+"</span></div>");
+		}
+		if(type == 'system')
+		{
+			$('#message_box').append("<div class=\"system_msg\">"+umsg+"</div>");
+		}
+		
+		$('#message').val(''); //reset text
+	};
+	
+	websocket.onerror	= function(ev){$('#message_box').append("<div class=\"system_error\">Error Occurred - "+ev.data+"</div>");}; 
+	websocket.onclose 	= function(ev){$('#message_box').append("<div class=\"system_msg\">Connection Closed</div>");}; 
+});
+</script>
+<div class="chat_wrapper">
+<div class="message_box" id="message_box"></div>
+<div class="panel">
+<input type="text" name="name" id="name" placeholder="Your Name" maxlength="10" style="width:20%"  />
+<input type="text" name="message" id="message" placeholder="Message" maxlength="80" style="width:60%" />
+<button id="send-btn">Send</button>
 </div>
-    <div class="navbar-fixed-bottom" style="margin-bottom: -30px;margin-top: 0px;margin-left: 10px;margin-right: 20px;">
-        <form onsubmit="ws.send($('input').val()); $('input').val(''); return false; ">
-            <input id="input" type="text" class="form-control" placeholder="Text input" style="width: 100%;" maxlength="140" autocomplete="off">
-        </form>
-    </div>
+</div>
+
 </body>
 </html>
