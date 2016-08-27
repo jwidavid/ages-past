869dd393ec844d95b0bf300867c5363f2fb01d8d
Jared David <skynet@Jareds-MacBook-Pro.local>
2016-08-27 13:14:07 -0400

    Appearance changes

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -20,6 +20,26 @@
 
 
 @section("page-script-head")
+
+<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,200italic,300italic" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="http://d3dhju7igb20wy.cloudfront.net/assets/0-4-0/all-the-things.css" />
+    <style>
+        .chat-app {
+            margin: 50px;
+            padding-top: 10px;
+        }
+
+        .chat-app .message:first-child {
+            margin-top: 15px;
+        }
+
+        #messages {
+            height: 300px;
+            overflow: auto;
+            padding-top: 5px;
+        }
+    </style>
+
 <script src="https://cdn.rawgit.com/samsonjs/strftime/master/strftime-min.js"></script>
     <script src="//js.pusher.com/3.0/pusher.min.js"></script>
 
