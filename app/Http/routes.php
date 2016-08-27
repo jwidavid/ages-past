a99b15c13d1fe337d5a59c10aa331f75f38f914f
Jared David <skynet@Jareds-MacBook-Pro.local>
2016-08-27 14:08:40 -0400

    Removed testing Bridge for pusher

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,17 +17,17 @@ Route::get('/', function () {
 
 
 
-use Illuminate\Support\Facades\App;
-
-Route::get('/bridge', function() {
-    $pusher = App::make('pusher');
-
-    $pusher->trigger( 'test-channel',
-                      'test-event', 
-                      array('text' => 'Preparing the Pusher Laracon.eu workshop!'));
-
-    return view('index');
-});
+// use Illuminate\Support\Facades\App;
+// 
+// Route::get('/bridge', function() {
+//     $pusher = App::make('pusher');
+// 
+//     $pusher->trigger( 'test-channel',
+//                       'test-event', 
+//                       array('text' => 'Preparing the Pusher Laracon.eu workshop!'));
+// 
+//     return view('index');
+// });
 
 
 
