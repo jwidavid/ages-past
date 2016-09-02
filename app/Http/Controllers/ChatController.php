a2f86e23e0d5d0805d83cd24128325d3ce7c4b91
Jared David <skynet@Jareds-MacBook-Pro.local>
2016-09-01 22:08:16 -0400

    Changes to design

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -30,7 +30,7 @@ class ChatController extends Controller
 	    $newMessages = array();
 	    
 	    foreach ($messages as $message) {
-		    $message[5] = date('H:i:s', $message['created_at']);
+		    $message['created_at'] = date('H:i:s', $message['created_at']);
 		    $newMessages[] = $message;
 	    }
 	    
